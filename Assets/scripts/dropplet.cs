4b735e8b5ce5798664b2f8a7dab4936f495849d2
thomas2 <thomas2>
2016-04-16 13:37:40 -0600

    First pass at temp and porting over work from old project that wasn't in git.

---
+++ Assets/scripts/dropplet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class dropplet : MonoBehaviour {
+	public int temp;
+	public int heatstate;
+	// Use this for initialization
+	void Start () {
+		//temp = 100;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//temp = temp - 1;
+		//Debug.Log (temp);
+		//if (temp == 0) {
+		//	Debug.Log ("frozen");
+
+		//}
+		Debug.Log(heatstate);
+
+		}
+	void OnTriggerStay ( Collider col ){
+		Debug.Log (col.name);
+		if (col.gameObject.tag == "red_crystal") {
+			heatstate = 1;
+		} 
+		//if (col.gameObject.tag == "blue_crystal")
+
+	}
+}
