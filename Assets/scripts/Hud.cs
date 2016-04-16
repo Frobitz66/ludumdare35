b2b85110463dcf83ff33ca3e563a86440865b169
Caelrin <Andrea.G.Gibson@gmail.com>
2016-04-16 15:32:03 -0600

    add lives script.

---
+++ Assets/scripts/Hud.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hud : MonoBehaviour {
+
+	public GameObject life1;
+	public GameObject life2;
+	public GameObject life3;
+
+	public int lives = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lives < 3) {
+			life3.SetActive (false);
+		}
+		if (lives < 2) {
+			life2.SetActive (false);
+		}
+		if (lives < 1) {
+			life1.SetActive (false);
+		}
+	}
+
+	public void LoseLife() {
+		lives--;
+	}
+		
+}
