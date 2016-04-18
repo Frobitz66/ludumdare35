f7093d38510672f6d16da082d70897b1604cfcf4
Nimgoble <nimgoble@gmail.com>
2016-04-17 21:34:36 -0600

    Added the ability to switch on click

--- Assets/scripts/SwitchLevel.cs
+++ Assets/scripts/SwitchLevel.cs
@@ -5,10 +5,23 @@ using System.Collections;
 public class SwitchLevel : MonoBehaviour {
 
 	public string LevelToSwitchTo = string.Empty;
+	public bool SwitchOnClick = false;
 
 	void OnTriggerEnter2D(Collider2D coll)
 	{
+		if (SwitchOnClick)
+			return;
+		
 		GameState.droplet = coll.gameObject;
 		SceneManager.LoadScene(LevelToSwitchTo);
+	}
+
+	void OnMouseOver()
+	{
+		if (Input.GetMouseButtonDown(0) && SwitchOnClick)
+		{
+			SceneManager.LoadScene(LevelToSwitchTo);    
+
+		}
 	}
 }
