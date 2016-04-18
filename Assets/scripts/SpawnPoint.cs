112b9023114e758934590a0c0b9a4be515524b34
Nimgoble <nimgoble@gmail.com>
2016-04-17 21:21:26 -0600

    Added spawn point script and prefab

---
+++ Assets/scripts/SpawnPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (GameState.droplet != null)
+		{
+			Debug.Log("Setting droplet start pos: " + GetComponent<Transform>().position);
+			GameState.droplet.transform.position = GetComponent<Transform>().position;
+			Camera.main.GetComponent<SmoothCamera>().target = GameState.droplet;
+		}
+	}
+}
