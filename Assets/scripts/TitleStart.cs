4a1849aba13a1bb11f9ef08d6732f5f521f72f27
tgruenewald <terrygruenewald@gmail.com>
2016-04-18 05:56:29 -0600

    NEW FINAL LUDUM DARE SUBMISSION:  Added credits button to front page.  Made jungle level slightly easier to make it through.

--- Assets/scripts/TitleStart.cs
+++ Assets/scripts/TitleStart.cs
@@ -3,9 +3,14 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class TitleStart : MonoBehaviour {
-
+    public Canvas creditCanvas;
     public void clickStart()
     {
         SceneManager.LoadScene("Opening 1");
+    }
+
+    public void clickCredits()
+    {
+        creditCanvas.enabled = true;
     }
 }
