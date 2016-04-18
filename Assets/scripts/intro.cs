b0a1d6b6997e91f4dbb3a37135c9b0a1ba0dc396
thomas2 <thomas2>
2016-04-17 19:41:06 -0600

    intro

---
+++ Assets/scripts/intro.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class intro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseOver()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			SceneManager.LoadScene("opening 2");    
+
+		}
+	}
+
+
+} 
---
+++ Assets/scripts/intro4.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class intro4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseOver()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			SceneManager.LoadScene("opening 4");    
+
+		}
+	}
+
+
+} 
